520505f9ec9dc5bae6c882d9f0bd4aac319da264
MateoClausen14 <mclausen@mail.austral.edu.ar>
2026-03-31 21:45:12 -0300

    Add files via upload

--- names.py
+++ names.py
@@ -2,12 +2,12 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+    nombre = input()
+    apellido = input()
 
-    nombre=input("Ingrese su nombre")
-    apellido=input("Ingrese su apellido")
-    nya=nombre+" "+apellido
-    print(nya.lower())
-    print(nya.title())
-    print(nya.upper())
-    #print("\t",nya.lower())
-    print(f"\t{nya.lower()}")
+    completo = nombre + " " + apellido
+
+    print(completo.lower())
+    print(completo.title())
+    print(completo.upper())
+    print("\t" + completo.lower())
